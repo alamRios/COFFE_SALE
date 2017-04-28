54d15dea271e457c55e46c6f92d072a96c5d0c84
alamRios <alam.rios@ymail.com>
2017-04-28 12:51:37 -0500

    Agregando projecto para STS

---
+++ CODIGO/STSProject/COFFE_SALE/src/main/java/hello/GreetingController.java
@@ -0,0 +1,25 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * @author alamRios
+ * Esta clase es un WSRest (Notar la anotación)
+ * */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    /**
+     * Servicio GET no se especifica porque es default para RequestMapping
+     * */
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));//Es devuelto como JSON por Jackson2
+    }
+}
