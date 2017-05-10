bb63dfbec2479289088698577b3e89cb49587ee2
alamRios <alam.rios@ymail.com>
2017-05-10 11:29:51 -0500

    Implementacion JDBC: Cafeterias

---
+++ CODIGO/STSProject/COFFE_SALE/src/main/java/coffeSale/util/Constantes.java
@@ -0,0 +1,10 @@
+package coffeSale.util;
+
+public class Constantes {
+	public final static String dbManager = "mysql";
+	public final static String dbPath = "localhost";
+	public final static String dbPort = "4343";
+	public final static String dbName = "coffeSaleDB";
+	public final static String userDB = "coffeSale";
+	public final static String passDB = "c0ff3S4l3@POO";
+}
