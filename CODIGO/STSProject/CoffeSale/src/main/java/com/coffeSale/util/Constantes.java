5ae3936008bcef82b0e3e3be9fec571bf0bb5bf6
alamRios <alam.rios@ymail.com>
2017-05-12 12:56:03 -0500

    Cambiando rutas de BD

--- CODIGO/STSProject/CoffeSale/src/main/java/com/coffeSale/util/Constantes.java
+++ CODIGO/STSProject/CoffeSale/src/main/java/com/coffeSale/util/Constantes.java
@@ -2,9 +2,10 @@ package com.coffeSale.util;
 
 public class Constantes {
 	public final static String dbManager = "mysql";
-	public final static String dbPath = "localhost";
-	public final static String dbPort = "4343";
-	public final static String dbName = "coffeSaleDB";
-	public final static String userDB = "coffeSale";
+	public final static String dbPath = "db4free.net";
+	public final static String dbPort = "3306";
+	public final static String dbName = "coffesaledb";
+	public final static String userDB = "coffesale";
 	public final static String passDB = "c0ff3S4l3@POO";
+	// Se creo una cuenta en db4free.net para alojar la base de datos
 }
