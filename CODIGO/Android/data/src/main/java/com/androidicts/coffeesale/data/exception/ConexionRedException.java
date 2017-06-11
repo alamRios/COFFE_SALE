aaa770518ce44db2c6c761dc95de9f484e224345
Alex Maguey <alexmaguey1@gmail.com>
2017-06-11 03:47:12 -0500

    Added exception

---
+++ CODIGO/Android/data/src/main/java/com/androidicts/coffeesale/data/exception/ConexionRedException.java
@@ -0,0 +1,17 @@
+package com.androidicts.coffeesale.data.exception;
+
+/**
+ * Created by alejandro on 10/06/17.
+ * Androidicts.com
+ */
+
+public class ConexionRedException extends Exception {
+
+  public ConexionRedException() {
+    super();
+  }
+
+  public ConexionRedException(Throwable cause) {
+    super(cause);
+  }
+}
